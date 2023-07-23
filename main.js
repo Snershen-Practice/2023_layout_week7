558a828524a341be12f70f8f84ac66544d3e0ce2
Wu Jane <jane72418@gmail.com>
2023-07-23 21:34:35 +0800

    add AOS

---
+++ main.js
@@ -0,0 +1,49 @@
+import "./assets/scss/all.scss";
+import bootstrap from "bootstrap/dist/js/bootstrap";
+// import { Modal } from "bootstrap";
+
+let path = location.pathname;
+let pathArr = path.split("/");
+let pathName = pathArr[pathArr.length - 1];
+let htmlName = pathName.slice(0, pathName.indexOf(".html"));
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  if (htmlName === "index") {
+    const memberModal = new bootstrap.Modal("#member-modal");
+    memberModal.show();
+  }
+});
+
+(() => {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+AOS.init({
+  duration: 500,
+  easing: "ease-out-quart",
+});
+
+gsap.from(".text-scroll", { xPercent: -100, duration: 10, repeat: -1, ease: "linear" });
+gsap.from(".text-wrapper", { xPercent: -50 });
+
+// });
+// console.log("Hello world!");
